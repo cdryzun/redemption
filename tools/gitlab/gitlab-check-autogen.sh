dea27861854c61557d550eb3725aa9b097132fa6
Jonathan Poelen <jpoelen@wallix.com>
2025-08-27 18:40:16 +0200

    [ci] gitlab-check-autogen.sh: fix error propagation
    Add && on each command in the checker functions because 'set -e' is ignored in a function.

--- tools/gitlab/gitlab-check-autogen.sh
+++ tools/gitlab/gitlab-check-autogen.sh
@@ -6,7 +6,7 @@ set -ex
 
 check_gen_po()
 {
-    bjam update-po
+    bjam update-po &&
     if grep -E '^([-+]#: |@@|[-+]"|diff |index |[-]{3} |[+]{3})' -qv < <(
         git diff --unified=0 ./tools/i18n/po/*/
     ); then
@@ -17,16 +17,16 @@ check_gen_po()
 
 check_gen_logid()
 {
-    ./tools/log_siem/extractor.py -Cp > "$TMPDIR_TEST"/siem_filters_rdp_proxy.py
+    ./tools/log_siem/extractor.py -Cp > "$TMPDIR_TEST"/siem_filters_rdp_proxy.py &&
     diff ./tools/log_siem/siem_filters_rdp_proxy.py "$TMPDIR_TEST"/siem_filters_rdp_proxy.py
 }
 
 check_gen_config()
 {
     (
-        cd projects/redemption_configs
-        bjam
-        declare r=$(git status -s . 2>&1)
+        cd projects/redemption_configs &&
+        bjam &&
+        declare r=$(git status -s . 2>&1) &&
         if [[ -n $r ]]; then
             echo "$r"
             return 1
